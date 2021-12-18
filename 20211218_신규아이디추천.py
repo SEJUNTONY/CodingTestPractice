82b461f0b6c88a003a6fa2c0604d153b2e7026b1
SEJUNTONY <tonypark0723@gmail.com>
2021-12-18 22:52:46 +0900

    20211218 2nd submission - unfinished

--- 20211218_신규아이디추천.py
+++ 20211218_신규아이디추천.py
@@ -19,11 +19,16 @@ def solution(new_id):
                 q += 1
             elif new_id[q] == "." :
                 q += 1
+            elif new_id[q] == " " :
+                q += 1
             else :
                 new_id = new_id[:q] + "" + new_id[q+1:]
 
         else :
             q += 1
+    
+    if new_id == "" :
+        new_id = " "
 
     # 3단계
     i = 0 # Reset
@@ -31,57 +36,65 @@ def solution(new_id):
     length = len(new_id) # Reset
     for i in range (length) :
         if q >= len(new_id) :
-            return new_id
+            break
         if q == 0 :
             if new_id[q] == "." :
-                if new_id[q+1] == "." :
+                if len(new_id) == 1 :
+                    break
+                elif new_id[q+1] == "." :
                     new_id = new_id[:q] + "" + new_id[q+1:]
         if q >= 0 :
             if new_id[q] == "." :
                 if q == len(new_id)-1 :
-                    new_id = new_id[:q]
-                if new_id[q+1] == "." :
+                    new_id = new_id[:q+1]
+                elif new_id[q+1] == "." :
                     new_id = new_id[:q] + "" + new_id[q+1:]
-                if new_id[q-1] == "." :
-                    new_id = new_id[:q-1] + "" + new_id[q:]
-        q += 1
 
-    """
+        q += 1
+    
+    if new_id == "" :
+        new_id = " "
+
     # 4단계
     i = 0 # Reset
     q = 0 # Reset
     length = len(new_id) # Reset
-    if new_id[1] == "." :
+    if new_id[0] == "." :
         new_id = new_id[1:]
-    if new_id[len(new_id)-1] == "." :
-        new_id = new_id[:len(new_id)-1]
-    """
+    for i in range (len(new_id)) :
+        if new_id[len(new_id)-1] == "." :
+            new_id = new_id[:len(new_id)-1]
+        else :
+            break
 
-    """
+    if new_id == "" :
+        new_id = " "
+
     # 5단계
     i = 0 # Reset
     q = 0 # Reset
     length = len(new_id) # Reset
     for i in range (length) :
         if q >= len(new_id) :
-            return new_id
+            break
         if new_id[q] == " " :
             new_id = new_id[:q] + "a" + new_id[q+1:]
         q += 1
     
+
     # 6단계
     i = 0 # Reset
     q = 0 # Reset
     length = len(new_id) # Reset
     if len(new_id) >= 16 :
         new_id = new_id[:15]
-    q = 14
-    for i in range(16) :
-        if new_id[q] == "." :
-            new_id = new_id[:q]
-            q -= 1
-        else :
-            break
+        q = 14
+        for i in range(16) :
+            if new_id[q] == "." :
+                new_id = new_id[:q]
+                q -= 1
+            else :
+                break
     
     # 7단계
     i = 0 # Reset
@@ -91,10 +104,9 @@ def solution(new_id):
         new_id = new_id[0] + new_id[0] + new_id[0]
     if len(new_id) == 2 :
         new_id = new_id[0] + new_id[1] + new_id[1]
-    """
 
     answer = new_id
     return answer
 
-new_id = "...!@BaT#*..y.abcdefghijklm."
+new_id = "=.="
 print(solution(new_id))
